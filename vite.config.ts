71c052900853613e922298a291cfa8222959ad78
bartoszgolebiowski <bartosz.golebiowski24@gmail.com>
2025-03-06 12:04:03 +0100

    fix: enhance build configuration and add node polyfills support

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,29 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'zcsv',
-            fileName: 'zcsv',
+            fileName: (format) => {
+                if (format === 'es') return 'zcsv.mjs';
+                if (format === 'cjs') return 'zcsv.cjs';
+                return `zcsv.${format}.js`;
+            },
+            formats: ['es', 'cjs', 'umd']
         },
         rollupOptions: {
-            external: ['zod']
+            external: ['zod'],
         }
     },
-    plugins: [dts()],
-    test: {},
+    plugins: [
+        dts(),
+        nodePolyfills(),
+    ],
+    test: {
+        exclude: ['**/node_modules/**', '**/dist/**', '**/test-imports/**']
+    }
 });
